fda11e1f4f2fa4ec0c19862368352153efcf1abb
hstallworth3762 <hstallworth3@gatech.edu>
2016-10-03 13:12:48 -0400

    Added more javadocs

--- src/model/AccountTracker.java
+++ src/model/AccountTracker.java
@@ -7,10 +7,21 @@ public class AccountTracker {
 
     private List<User> accounts;
 
+    /**
+     * Creates new account tracker
+     */
     public AccountTracker() {
         accounts = new ArrayList<>();
     }
 
+    /**
+     * Adds a new account
+     *
+     * @param username the username
+     * @param password the password
+     * @param accountType the account type
+     * @return whether or not it was added
+     */
     public boolean addAccount(String username, String password, AccountType accountType) {
         for (User account: accounts) {
             if (account.getUsername().equals(username)) {
@@ -21,6 +32,13 @@ public class AccountTracker {
         return true;
     }
 
+    /**
+     * Checks if the account is in the tracker
+     *
+     * @param username the username
+     * @param password the password
+     * @return whether or not the acocunt was found
+     */
     public boolean checkAccount(String username, String password) {
         for (User account: accounts) {
             if (account.getUsername().equals(username) && account.getPassword().equals(password)) {
@@ -30,6 +48,12 @@ public class AccountTracker {
         return false;
     }
 
+    /**
+     * Returns user
+     *
+     * @param username the username
+     * @return the user
+     */
     public User getUser(String username) {
         for (User user: accounts) {
             if (user.getUsername().equals(username)) {

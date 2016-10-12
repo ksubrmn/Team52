fd807cc3b0eab0dfb4386eda28c7ee40a98faca6
ksubrmn <ksubramanian33@gatech.edu>
2016-10-12 13:35:17 -0400

    water report tracker

---
+++ src/model/WaterReportTracker.java
@@ -0,0 +1,15 @@
+package model;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kalyasubramanian1 on 10/12/16.
+ */
+public class WaterReportTracker {
+    private List<WaterSourceReport> reports;
+
+
+    public WaterReportTracker() {
+        reports = new ArrayList<>();
+    }
+}

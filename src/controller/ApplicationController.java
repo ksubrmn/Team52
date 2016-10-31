dd7c76492897a48da9885077aa04a7293aa95fe2
tina <christinachung2014@gmail.com>
2016-10-31 12:56:05 -0400

    Submit Purity Report is now only accessible to Workers and Managers

--- src/controller/ApplicationController.java
+++ src/controller/ApplicationController.java
@@ -4,7 +4,9 @@ import fxapp.Main;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import model.AccountType;
 
 public class ApplicationController {
 
@@ -39,7 +41,16 @@ public class ApplicationController {
     }
 
     public void HandleSubmitPurityReportButton() {
-        main.showSubmitPurityReportScreen();
+        if (main.getUser().getAccountType() == AccountType.Worker || main.getUser().getAccountType() == AccountType.Manager){
+            main.showSubmitPurityReportScreen();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Access Denied");
+            alert.setHeaderText("Access Denied");
+            alert.setContentText("Only Workers and Managers can create Water Purity Reports.");
+            alert.showAndWait();
+        }
+
     }
 
     public void HandleViewReportButton() {

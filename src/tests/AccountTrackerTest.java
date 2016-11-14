2e1f3eef650766d58156b6bd3ced6023bf4237b1
ksubrmn <ksubramanian33@gatech.edu>
2016-11-14 13:22:18 -0500

    created junit test

---
+++ src/tests/AccountTrackerTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import model.AccountTracker;
+import model.AccountType;
+import model.User;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kalyasubramanian1 on 11/14/16.
+ */
+public class AccountTrackerTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void addAccount() throws Exception {
+        AccountTracker at = new AccountTracker();
+
+        assertTrue(at.addAccount("User", "Password", AccountType.User));
+        assertFalse(at.addAccount("User", "Password", AccountType.User));
+
+    }
+
+}

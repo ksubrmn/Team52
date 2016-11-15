db4b09d85fb1b0a6d8eab2b5625a18b699ab1f14
hstallworth3762 <hstallworth3@gatech.edu>
2016-11-15 16:45:54 -0500

    Added addSourceReport JUnit test

---
+++ src/tests/SourceReportTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import model.*;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Helen Stallworth on 11/15/2016.
+ */
+public class SourceReportTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @org.junit.Test
+    public void addAccount() throws Exception {
+        WaterReportTracker tracker = new WaterReportTracker();
+        LocalDate date = null;
+
+        assertTrue(tracker.addSourceReport(date, "12:00", 10, 16, "Username",
+                WaterType.Bottled, WaterCondition.Potable));
+        assertFalse(tracker.addSourceReport(date, "12:00", 10, 16, "Username",
+                WaterType.Bottled, WaterCondition.Potable));
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+}

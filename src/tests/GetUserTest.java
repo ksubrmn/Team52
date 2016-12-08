3daac5be84e08baf9ff642edf54e9cecec4a0d1f
ksubrmn <ksubramanian33@gatech.edu>
2016-12-08 13:47:06 -0500

    get user test

---
+++ src/tests/GetUserTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import model.AccountTracker;
+import model.AccountType;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kalya Subramanian on 11/14/16.
+ * Tests to check correctness of AccountTracker#addAccount
+ */
+public class GetUserTest {
+    /**
+     @ Before
+     public void setUp() throws Exception {
+
+
+     }
+
+     @ After
+     public void tearDown() throws Exception {
+
+     }
+     */
+
+    @org.junit.Test
+    public void getUser() {
+        AccountTracker at = new AccountTracker();
+
+        assertTrue(at.addAccount("User", "Password", AccountType.User));
+        assertTrue(at.getUser("User") != null);
+
+    }
+
+}
